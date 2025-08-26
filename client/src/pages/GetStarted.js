82542f70fb3fa083a283a29a522720443d01064d
arpitKakran <arpitinsan77@gmail.com>
2025-08-27 00:54:01 +0530

    Add AppContext for user state management and update dependencies

--- client/src/pages/GetStarted.js
+++ client/src/pages/GetStarted.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const GetStarted = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default GetStarted
